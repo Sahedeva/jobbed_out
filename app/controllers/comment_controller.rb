0a8c435e377da1ed87a8cdbab3b73a26a38e4f32
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-09-27 16:14:54 -0500

    Got comment display function. Will say no comment if no comments for that field. Numbered postings for display.

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -5,11 +5,26 @@ class CommentController < ApplicationController
 	end
 
 	def new
-	
+		@comment = Comment.new
+		current_user
 	end
 
 	def create
 
 	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:comment,:date_time,:job_posting_id,:user_id)
+	end
+
+	private
+
+  def require_login
+    unless current_user
+      redirect_to root_url, notice: 'You need to be logged in to post!'
+  	end
+  end
 
 end
